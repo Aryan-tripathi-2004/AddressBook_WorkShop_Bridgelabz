aef23c7632f1e75ac485257723e9c2ffce706bd7
Om Datt Tripathi <aryantripathi1606@gmail.com>
2025-03-08 11:20:46 +0530

    [Om Datt Tripathi] init. initial commit

---
+++ src/main/java/com/example/AddressBookWorkShop/AddressBookWorkShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.AddressBookWorkShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookWorkShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookWorkShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AddressBookWorkShop/AddressBookWorkShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AddressBookWorkShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookWorkShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
