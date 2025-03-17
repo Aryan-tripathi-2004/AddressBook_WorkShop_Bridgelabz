396cb52a05cd8a3bf8b0a539909d3a4a0590b2e6
Om Datt Tripathi <aryantripathi1606@gmail.com>
2025-03-17 14:43:13 +0530

    [Om Datt tripathi] Add. user registration and authentication

--- src/main/java/com/example/AddressBookWorkShop/repository/AddressBookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.AddressBookWorkShop.repository;
-
-import com.example.AddressBookWorkShop.models.AddressBookEntry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressBookRepository extends JpaRepository<AddressBookEntry, Integer> {
-}
--- src/test/java/com/example/addressbook/AddressBookApplicationTests.java
+++ src/test/java/com/example/addressbook/AddressBookApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.AddressBookWorkShop;
+package com.example.addressbook;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AddressBookWorkShopApplicationTests {
+class AddressBookApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
